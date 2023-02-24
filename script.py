15765ee01e09b4beb44ab0eedb26f23c49778368
brianj04 <brianjtheawesome@gmail.com>
2023-02-24 12:02:29 -0600

    "Finished the Add Attraction func"

--- script.py
+++ script.py
@@ -10,5 +10,22 @@ def get_traveler_location(traveler):
     traveler_destination_index = get_desination_index(traveler_destination)
     return traveler_destination_index
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
 
+attractions = [[] for destinatio in destinations]
+def add_attraction(destination, attraction):
+    destination_index = get_desination_index(destination)
+    attractions_for_destinations = attractions[destination_index]
+    attractions[destination_index].append(attraction)
+    return attractions_for_destinations
+add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("Sao Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("Sao Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+print(attractions)
